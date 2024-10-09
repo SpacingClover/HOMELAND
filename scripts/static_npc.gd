9cc9f81f542df8390de33c6ea5259803fe65f845
Dominic Duncan <dominicduncan72@gmail.com>
2024-10-09 03:12:45 -0700

    movementdemo

--- scripts/static_npc.gd
+++ scripts/static_npc.gd
@@ -1,10 +1,30 @@
 class_name StaticNPC extends RoomItemInstance
 
+@onready var sprite : Sprite3D = $sprite
+
 var walking : bool = false
+var walklock : float
+
+@export var npc_script : Dictionary
+
+func _ready()->void:
+	pass
+	# read npc_script and set up signals
+	# create some way of the npc identifying the specific door it needs opened, and connecting to its door_opened signal
+	# the dict needs to contain a counter for which step it is on
 
 func _process(delta:float)->void:
 	if walking:
-		pass #animate feet
+		if walklock <= 0:
+			walklock = 0.1
+			if sprite.frame >= 3:
+				sprite.frame = 0
+			else:
+				sprite.frame += 1
+		else:
+			walklock -= 2 * delta
+	else:
+		sprite.frame = 0
 
 func interact()->void:
 	DEV_OUTPUT.push_message("npc interacted")
