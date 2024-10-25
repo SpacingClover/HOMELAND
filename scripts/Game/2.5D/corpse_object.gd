8f9a4bad81d024cd2ff6aeb0b3b3d007b5342034
Dominic Duncan <dominicduncan72@gmail.com>
2024-10-25 10:55:31 -0700

    movementdemo

--- scripts/Game/2.5D/corpse_object.gd
+++ scripts/Game/2.5D/corpse_object.gd
@@ -10,11 +10,12 @@ enum{
 	FACE_SHOT,
 	LEG_SHOT,
 	PELVIS_EXPLODED,
-	FACE_EXPLODED
+	FACE_EXPLODED9
 }
 
 var team : int = ENEMY
 var blood_frame : int = CHEST_SHOT
+var flipped : bool = false
 
 func pass_args(args:Array=[])->void:
 	if args.size() >= 1:
@@ -25,6 +26,10 @@ func pass_args(args:Array=[])->void:
 		blood_frame = args[1]
 	else:
 		blood_frame = 0
+	if args.size() >= 3:
+		flipped = true
+	else:
+		flipped = false
 
 func _ready()->void:
 	if team != 0:
@@ -33,6 +38,9 @@ func _ready()->void:
 		$body.vframes = 3
 		$body.frame = 6
 	$blood.frame = blood_frame
+	if flipped:
+		$body.flip_h = true
+		$blood.flip_h = true
 
 func get_data()->RoomItem:
-	return RoomItem.new(item_id,position,rotation,[team,blood_frame])
+	return RoomItem.new(item_id,position,rotation,[team,blood_frame,flipped])
